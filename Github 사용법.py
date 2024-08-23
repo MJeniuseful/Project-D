b27f67ce19692c02599c127de34383cd5f9325a6
Minjae Kang <servral7@gmail.com>
2024-08-23 22:53:30 +0900

    사용방법

---
+++ Github 사용법.py
@@ -0,0 +1,33 @@
+## 맨 처음에 터미널을 먼저 열어야 함 ##
+
+## 1. 초기화
+# git init
+
+## 2. 추가할 파일 더하기
+# git add .
+
+## 3. 히스토리 만들기(지금 버전에 대한 코멘트 남기기)
+# git commit -m "코멘트"
+# ex) git commit -m "텔레그램 채팅기능 추가"
+
+## 4. Github Repository랑 연결
+# git remote add origin https://github.com/MJeniuseful/Project-D.git
+
+## 5. 잘 연결되었는지 확인
+# git remote -v
+
+## 6. 깃허브로 올리기
+# git push origin master
+
+
+#################### 이후 연결되었으면 아래 3가지 방법만 계속하면 됨 ##########
+
+## 1. 추가할 파일 더하기
+# git add .
+
+## 2. 히스토리 만들기(지금 버전에 대한 코멘트 남기기)
+# git commit -m "코멘트"
+
+# 3. 깃허브로 올리기
+# git push origin master
+
